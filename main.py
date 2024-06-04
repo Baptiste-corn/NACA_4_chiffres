2bbc817849abaa22436e4d6aa0be5fa54f924a2d
Baptiste-corn <61588561+Baptiste-corn@users.noreply.github.com>
2024-06-04 15:10:49 -0400

    Affichage complet, ajout du point de corde où l'épaisseur est maximale

--- main.py
+++ main.py
@@ -2,58 +2,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Initialisation des variables
+profil = ""
+chord = ""
+points = ""
+distribution = 0
+thickness = 0
 
-profil = str(input ("Veillez choisir votre profil NACA 4 chiffres : "))
-thickness = float(profil[2 :]) / 100  # Epaisseur de la corde en %
-print(thickness)
+# Saisi utilisateur des caractéristiques du profil NACA
 
-chord = int(input("Veillez choisir la taille de la corde en metre : "))
-points = int(input("Combien de points en abscisse souhaitez vous pour tracer le graphique ?"
-                             "\nVeillez choisir un nombre entier\n "))
+# Blindage pour s'assurer que l'utilisateur rentre 4 chiffres entiers positifs
+while len(profil) != 4 or profil.isnumeric() == False:
+    profil = str(input("Veillez choisir un profil NACA 4 chiffres : "))
 
-distribution = int(input("Pour finir, souhaitez vous une destribution de point linéaire ou"
-                     "non uniforme ? \n1 : Lineaire \n2 : Non uniforme\n"
-                     "Tapez 1 ou 2\n"))
+thickness = float(profil[2:]) / 100
+# Convertit la chaine de caractere en deux chiffres flottants et met en %
+# Recupere les 2 derniers chiffres saisis par l'utilisateur
 
+print(f"Epaisseur du profil = {thickness * 100}%")
+
+while not chord.isnumeric(): # Blindage pour s'assurer que l'utilisateur ne rentre que des entiers positifs
+    chord = str(input("Veillez choisir la taille de la corde en mètre : "))  # Taille de la corde
+chord = float(chord[::]) # On convertit la chaine de caractère saisie en nombre réel
+
+while not points.isnumeric(): # Même blindage pour n'avoir que des entiers positifs saisis
+    points = input('Combien de points souhaitez vous pour tracer le graphique ?'
+                   '\nVeillez choisir un nombre entier\n ')
+points = int(points[::]) # On convertit la chaîne saisie en nombre entier
+
+# Permet de choisir quelle distribution souhaite l'utilisateur, linéaire ou non uniforme
+# Blindage pour que l'utilisateur ne puisse saisir que 1 ou 2
+while distribution != 1 and distribution != 2:
+    distribution = int(input("Pour finir, souhaitez vous une distribution de point linéaire ou"
+                             "non uniforme ? \n1 : Linéaire \n2 : Non uniforme\n"
+                             "Tapez 1 ou 2\n"))
+
+###### Creation des arrays pour les tableaux de coordonnées du profil choisi par l'utilisateur
+
+# On crée un array allant de 0 à 1 avec un nombre de points choisi par l'utilisateur
 xc_linear = np.linspace(0, 1, points)
 
+# On fait la même chose pour la distribution non uniforme de 0 à pi
 teta = np.linspace(0, np.pi, points)
 xc_non_uniform = 0.5 * (1 - np.cos(teta))
 
-yt_linear = 5*thickness * (0.2969*np.sqrt(xc_linear) - 0.1260*xc_linear - 0.3516*xc_linear**2
-                            + 0.2843*xc_linear**3 - 0.1036*xc_linear**4)
+#### Calcul des demis-épaisseurs du profil avec les tableaux remplis précedemments
+yt_linear = 5 * thickness * (0.2969 * np.sqrt(xc_linear) - 0.1260 * xc_linear - 0.3516 * xc_linear ** 2
+                             + 0.2843 * xc_linear ** 3 - 0.1036 * xc_linear ** 4)
 
-yt_non_uniform = 5*thickness * (0.2969*np.sqrt(xc_non_uniform) - 0.1260*xc_non_uniform - 0.3516*xc_non_uniform**2
-                            + 0.2843*xc_non_uniform**3 - 0.1036*xc_non_uniform**4)
+yt_non_uniform = 5 * thickness * (
+        0.2969 * np.sqrt(xc_non_uniform) - 0.1260 * xc_non_uniform - 0.3516 * xc_non_uniform ** 2
+        + 0.2843 * xc_non_uniform ** 3 - 0.1036 * xc_non_uniform ** 4)
 
-x_up_linear = xc_linear*chord
-x_down_linear = xc_linear*chord
-y_up_linear = yt_linear*chord
-y_down_linear = -yt_linear*chord
+# Création des courbes du profil
+x_up_linear = xc_linear * chord
+x_down_linear = xc_linear * chord
+y_up_linear = yt_linear * chord
+y_down_linear = -yt_linear * chord
 
-x_up_non_uniform = xc_non_uniform*chord
-x_down_non_uniform = xc_non_uniform*chord
-y_up_non_uniform = yt_non_uniform*chord
-y_down_non_uniform = -yt_non_uniform*chord
-
+x_up_non_uniform = xc_non_uniform * chord
+x_down_non_uniform = xc_non_uniform * chord
+y_up_non_uniform = yt_non_uniform * chord
+y_down_non_uniform = -yt_non_uniform * chord
 
 ###### EPAISSEUR MAXIMUM ########
 thickness_max_position_linear = np.argmax(y_up_linear)
 thickness_max_position_non_uniform = np.argmax(y_up_non_uniform)
-print(f"Voici la position de l'épaisseur maximale le long de la corde"
-      f"\nDistribution linéaire  : {x_up_linear[thickness_max_position_linear]}, "
-      f"\nDistribution non uniforme : {x_up_non_uniform[thickness_max_position_non_uniform]}")
-print(f"La valeur de l'épaisseur maximale est : "
+
+print(f"La position de l'épaisseur maximale le long de la corde se situe à "
+      f"\n{x_up_linear[thickness_max_position_linear]} mètres pour une distribution linéaire," 
+      f"\n{x_up_non_uniform[thickness_max_position_non_uniform]} mètres pour une distribution non uniforme")
+
+print(f"\nLa valeur de l'épaisseur maximale est : "
       f"\nLinéaire_max = {y_up_linear[thickness_max_position_linear] * 2} mètres"
       f"\nNon_uniforme_max = {y_up_non_uniform[thickness_max_position_non_uniform] * 2} mètres")
 
 if distribution == 1:
     print("\nVous avez choisi une distribution linéaire\n")
     plt.axis('equal')
-    plt.plot(x_up_linear, y_up_linear)
-    plt.plot(x_down_linear, y_down_linear)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.plot(x_up_linear, y_up_linear, label='intrados')
+    plt.plot(x_down_linear, y_down_linear, label='extrados')
+    plt.scatter(x_up_linear[thickness_max_position_linear], y_up_linear[thickness_max_position_linear],
+                label='épaisseur maximale', color='green')
+    plt.scatter(x_up_linear[thickness_max_position_linear], -y_up_linear[thickness_max_position_linear],
+                color='green')
+    plt.xlabel('axe x')
+    plt.ylabel('axe y')
     plt.legend()
     plt.grid()
     plt.title(f'Profil NACA{profil} distribution linéaire')
@@ -62,10 +96,14 @@ if distribution == 1:
 elif distribution == 2:
     print("\nVous avez choisi une distribution non uniforme\n")
     plt.axis('equal')
-    plt.plot(x_up_non_uniform, y_up_non_uniform)
-    plt.plot(x_down_non_uniform, y_down_non_uniform)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.plot(x_up_non_uniform, y_up_non_uniform, label='intrados')
+    plt.plot(x_down_non_uniform, y_down_non_uniform, label='extrados')
+    plt.scatter(x_up_non_uniform[thickness_max_position_non_uniform], y_up_non_uniform[thickness_max_position_non_uniform],
+                label='épaisseur maximale', color='green')
+    plt.scatter(x_up_non_uniform[thickness_max_position_non_uniform], -y_up_non_uniform[thickness_max_position_non_uniform],
+                color='green')
+    plt.xlabel('axe x')
+    plt.ylabel('axe y')
     plt.legend()
     plt.grid()
     plt.title(f'Profil NACA{profil} distribution non uniforme')
@@ -73,6 +111,5 @@ elif distribution == 2:
 
 else:
     print("ce cas n'est pas prit en compte")
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
